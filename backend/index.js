2ba179afebd371643980bfaaaf542e91597605a4
anikmahidul9 <mahidulanik9@gmail.com>
2025-04-26 14:46:44 +0600

    Revert "backend udate"
    This reverts commit e00d140810ccc9281b727c79bfe52dfe47cf784c.

--- backend/index.js
+++ backend/index.js
@@ -27,23 +27,12 @@ const allowedOrigins = [
     "http://localhost:5173",                  // Local dev
   ];
   
-
-  app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Enable if using cookies/sessions
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-  
+const corsOptions = {
+    origin: allowedOrigins,
+   
+    credentials: true,
+}
+app.use(cors(corsOptions));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/api/v1/user",userRoute);
